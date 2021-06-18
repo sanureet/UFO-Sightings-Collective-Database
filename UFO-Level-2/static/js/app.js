e12bf7e9573a383954d9deef6db4c5d786466843
sanureet <sanureetbhullar@gmail.com>
2021-06-17 20:37:11 -0700

    started level 2

--- UFO-Level-2/static/js/app.js
+++ UFO-Level-2/static/js/app.js
@@ -2,3 +2,10 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+console.log(tableData);
+
+var tbody = d3.select("tbody");
+var filtered = 0;
+var columns = ["datetime,"city", "state", "country", "shape"]
+
+
